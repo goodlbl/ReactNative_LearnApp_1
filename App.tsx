656aab7f72603a10e131b231fbd5eb3d1abb1977
goodlbl <Goodworldxyz@gmail.com>
2025-05-11 22:28:02 +0700

    vi5

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 
 export default function App() {
+
+  const [num, setNum] = useState<number>(0);
+  const [str, setStr] = useState({
+    name: "Friend",
+    age: 25,
+  });
+
+  const forNum = () => {
+    setNum(num + 1);
+    alert(num);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text style={{color: "pink",
-      fontSize: 30,
-      borderWidth: 1
-      }}>Kia con buom vang</Text>
-      <View>
-        <Text style={styles.hello}>Tai sao the?</Text>
-        <Text style={styles.hello}>
-          <Text>hai{"\n"}</Text>
-          <Text>ko hai</Text>
-        </Text>
+      <Text style={styles.ume}>
+        <Text>Open up App.tsx to start working on your app!{"\n\n"}</Text>
+        <Text>{num}{"\n\n"}</Text>  
+        <Text>{str.name}</Text>
+      </Text>
+      <View style={styles.upbutton}>
+        <Button title='Up' onPress={() => setNum(num + 1)}/>
+        <Button title="Up and on" onPress={() => forNum()}/>
       </View>
     </View>
   );
@@ -25,14 +35,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#9f9',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  hello: {
-    color: "red",
-    fontSize: 25,
-    fontWeight: "bold",
-    textAlign:"center"
+  ume: {
+    textAlign: "center",
+    fontSize: 16
   },
+  upbutton: {
+    backgroundColor: "#bde"
+  }
 });
