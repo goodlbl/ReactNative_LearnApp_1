b239feed83c55bef561bd6b2898092db45af37f5
Goodlbl <goodworldxyz@gmail.com>
2026-04-30 13:47:43 +0700

    change flexbox view 4

--- components/flexbox.tsx
+++ components/flexbox.tsx
@@ -14,6 +14,10 @@ const FlexBox = () => {
             <View style={styles.i3}>
                 <Text>Ik 3</Text>
             </View>
+
+            <View style={styles.i3}>
+                <Text>Ik 4</Text>
+            </View>
         </View>
     );
 };
